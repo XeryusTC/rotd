2ce3b7bb5207fcdbedd731bb9cbc928393654507
XeryusTC <armadillo@onenetbeyond.org>
2015-07-16 15:03:58 +0200

    Add functional test to test the home page

--- functional_tests/test_homepage.py
+++ functional_tests/test_homepage.py
@@ -8,9 +8,16 @@ class HomePageRecipeTests(unittest.TestCase):
     def tearDown(self):
         self.browser.close()
 
-    def test_django_working(self):
+    def test_can_see_todays_recipe(self):
+        # Alice goes to our website
         self.browser.get('http://localhost:8000')
-        self.assertIn('Django', self.browser.title)
+
+        # She notices that the title says 'Recept van de dag'
+        self.assertIn('Recept van de dag', self.browser.title)
+
+        # There is also a header on the page that says 'Recept van de dag'
+        header_text = self.browser.get_element_by_tag_name('h1')
+        self.assertIn('Recept van de dag', header_text.text)
 
 if __name__ == "__main__":
     unittest.main()
