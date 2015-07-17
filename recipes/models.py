acf244b72c3bb4ea7c7298edcc2e1904bf396d73
XeryusTC <armadillo@onenetbeyond.org>
2015-07-17 16:14:49 +0200

    Make sure that the recipe is unique

--- recipes/models.py
+++ recipes/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=64, blank=False, default='')
+    name = models.CharField(max_length=64, blank=False, default='',
+            unique=True)
 
     def __str__(self):
         return self.name
--- recipes/tests/test_models.py
+++ recipes/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
+from django.db.utils import IntegrityError
 
 from recipes.models import Recipe
 
@@ -11,6 +12,12 @@ class RecipeModelTests(TestCase):
         r = Recipe.objects.create(name='')
         with self.assertRaises(ValidationError):
             r.full_clean()
+
+    def test_recipe_name_is_unique(self):
+        r1 = Recipe.objects.create(name='test recipe')
+        r1.save()
+        with self.assertRaises(IntegrityError):
+            r2 = Recipe.objects.create(name='test recipe')
 
     def test_string_representation(self):
         recipe = Recipe.objects.create(name='test recipe')
