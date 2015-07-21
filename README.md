bd87fb41d269c0723662cbd370a80bf3cdeaf38b
XeryusTC <armadillo@onenetbeyond.org>
2015-07-21 19:15:27 +0200

    Make provisioning and deployment script

--- README.md
+++ README.md
@@ -3,6 +3,12 @@ Recipe of the Day
 
 A website which shows a new recipe to try out every day.
 
+Licence
+-------
+
+This project currently has no licence, which means that it falls under
+copyright law.
+
 Provisioning
 ------------
 
@@ -18,6 +24,16 @@ To get them on Debian:
 	sudo apt-get install nginx git python3 python3-pip \
 		postgresql-server-dev-9.4
 	sudo pip3 install virtualenv
+
+There are two template files in deploy\_tools that currently need editing by
+hand. In the future this can be done by running the 'fab provision' command.
+Currently you need to replace every instance of SITENAME in the files and
+filenames with the domain name of the site
+
+deploy\_tools/nginx.conf.template:
+	move to /etc/nginx/sites-available/SITENAME
+	update settings in /etc/nginx/sites-available/SITENAME
+	make symlink in /etc/nginx/sites-enabled/ to the file
 
 
 Installing
---
+++ fabfile.py
@@ -0,0 +1,131 @@
+from fabric.api import env, local, prompt, settings, sudo, run
+from fabric.contrib.files import append, exists, sed
+from getpass import getpass
+import random
+
+REPO_URL = 'https://github.com/XeryusTC/rotd.git'
+
+dest_folder = None
+source_folder = None
+db_name = None
+db_user = None
+db_pass = None
+
+def provision():
+    sudo('apt-get install nginx git python3 python3-pip \
+            postgresql-server-dev-9.4')
+    sudo('pip3 install virtualenv')
+    _setup_variables(True)
+
+    # Test database if user exists, if not then create it
+    db_setup = sudo("psql -tAc \"SELECT 1 FROM pg_roles WHERE rolname='%s'\"" \
+            % (db_user,), user='postgres') == '1'
+    if not db_setup:
+        sudo("psql -c \"CREATE USER %s WITH PASSWORD '%s'\"" % (db_user, db_pass),
+                user='postgres')
+    # Test if database is set up, if not create it and give user access
+    db_exists = sudo('psql -lqt | cut -d \| -f 1 | grep -w %s | wc -l' %
+            (db_name,), user='postgres') == '1'
+    if not db_exists:
+        sudo('psql -c "CREATE DATABASE %s"' % (db_name,), user='postgres')
+        sudo('psql -c "GRANT ALL PRIVILEGES ON DATABASE %s TO %s"' % (db_name,
+            db_user), user='postgres')
+
+    # Make sure all files are downloaded and Django is set up
+    deploy()
+
+    # Set up nginx
+    run('cp %s/deploy_tools/nginx.conf.template /tmp/' % (source_folder,))
+    sed('/tmp/nginx.conf.template', 'SITENAME', env.host)
+    sudo('mv /tmp/nginx.conf.template /etc/nginx/sites-available/%s' % (
+        env.host,))
+
+    # set up systemd to run the gunicorn service
+    gunicorn_template = 'gunicorn-systemd.service.template'
+    run('cp %s/deploy_tools/%s /tmp/' % (source_folder, gunicorn_template))
+    sed('/tmp/%s' % (gunicorn_template,), 'SITENAME', env.host)
+    sudo('mv /tmp/%s /etc/systemd/system/gunicorn-%s.service' % (
+        gunicorn_template, env.host))
+
+def deploy():
+    _setup_variables()
+    _create_folder_structure(dest_folder)
+    _get_latest_source(source_folder)
+    _update_virtualenv(dest_folder)
+    _update_settings(source_folder, env.host)
+    _update_static_files(source_folder)
+    _update_database(source_folder)
+
+
+def _setup_variables(database=False):
+    global dest_folder, source_folder
+    if dest_folder != None:
+        return
+    run('uptime') # make sure that we have a host set
+    dest_folder = '/var/www/sites/%s' % (env.host,)
+    source_folder = dest_folder + '/source'
+    if database:
+        _setup_database_variables()
+
+def _setup_database_variables():
+    global db_name, db_user, db_pass
+    db_name = prompt('Database name: ', default='rotd')
+    db_user = prompt('Database user: ', default='rotd')
+    db_pass = getpass('Database password: ')
+
+
+def _create_folder_structure(folder):
+    for subfolder in ('static', 'virtualenv', 'source'):
+        sudo('mkdir -p %s/%s' % (folder, subfolder))
+        sudo('chown -R %s:www-data %s' % (env.user, folder,))
+
+def _get_latest_source(source_folder):
+    if exists('%s/.git' % (source_folder,)):
+        run('cd %s && git fetch' % (source_folder,))
+    else:
+        run('git clone %s %s' % (REPO_URL, source_folder))
+    current_commit = local('git log -n 1 --format=%H', capture=True)
+    run('cd %s && git reset --hard %s' % (source_folder, current_commit))
+
+def _update_settings(source_folder, site_name):
+    settings_path = source_folder + '/rotd/settings.py'
+    sed(settings_path, 'DEBUG = True', 'DEBUG = False')
+    sed(settings_path, 'DOMAIN = "localhost"', 'DOMAIN = "%s"' % site_name)
+
+    # Set up secret key
+    secret_key_file = source_folder + '/rotd/secret_key.py'
+    if not exists(secret_key_file):
+        chars = 'qwertyuiopasdfghjklzxcvbnmQWERTYIOPASDFGHJKLZXCVBNM' + \
+                '1234567890!@#$%^&*()_+-='
+        key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
+        append(secret_key_file, 'SECRET_KEY = "%s"' % (key,))
+    append(settings_path, '\nfrom .secret_key import SECRET_KEY')
+
+    # Set up database
+    database_file = source_folder + '/rotd/database.py'
+    if not exists(database_file):
+        if db_name is None:
+            _setup_database_variables()
+
+        run('cd %s && cp deploy_tools/database.py %s' % (source_folder,
+            database_file))
+        sed(database_file, '"NAME": ""', '"NAME": "%s"' % (db_name,))
+        sed(database_file, '"USER": ""', '"USER": "%s"' % (db_user,))
+        sed(database_file, '"PASSWORD": ""',
+            '"PASSWORD": "%s"' % (db_pass,))
+    append(settings_path, '\nfrom .database import DATABASES')
+
+def _update_virtualenv(folder):
+    virtualenv_folder = folder + '/virtualenv'
+    if not exists(virtualenv_folder + '/bin/python'):
+        run('virtualenv --python=python3 %s' % (virtualenv_folder,))
+    run('%s/bin/pip install -r %s/requirements.txt' % (virtualenv_folder,
+        folder + '/source'))
+
+def _update_static_files(source_folder):
+    run('cd %s && ../virtualenv/bin/python3 manage.py collectstatic \
+            --noinput' % (source_folder,))
+
+def _update_database(source_folder):
+    run('cd %s && ../virtualenv/bin/python3 manage.py migrate --noinput' % (
+        source_folder,))
