0995a28bf9b552da0af3219c31b684adc851eb67
XeryusTC <armadillo@onenetbeyond.org>
2015-07-22 15:26:23 +0200

    Make the home page a bit more sane

--- recipes/tests/test_views.py
+++ recipes/tests/test_views.py
@@ -9,35 +9,46 @@ from recipes.views import home_page, todays_recipe
 from recipes import factories
 
 class HomePageViewTest(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        super(HomePageViewTest, cls).setUpClass()
-        factories.RecipeFactory()
+    def get_homepage_content(self):
+        request = HttpRequest()
+        return home_page(request)
 
     def test_root_url_resolves_to_home_page_view(self):
+        factories.RecipeFactory()
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_inherits_from_base_template(self):
+        factories.RecipeFactory()
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'rotd/base.html')
 
     def test_home_page_uses_correct_template(self):
+        factories.RecipeFactory()
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'recipes/home.html')
 
+    def test_home_page_degrades_gracefully_when_no_recipe(self):
+        response = self.get_homepage_content().content.decode()
+        self.assertNotIn('Exception', response)
+
     def test_home_page_has_recipe(self):
-        Recipe.objects.create(name='test')
+        factories.RecipeFactory()
         response = self.client.get('/')
         self.assertIsInstance(response.context['recipe'], Recipe)
 
     def test_home_page_shows_any_recipe_name(self):
-        Recipe.objects.create(name='test recipe')
-        request = HttpRequest()
-        response = home_page(request).content.decode()
+        factories.RecipeFactory()
+        response = self.get_homepage_content().content.decode()
 
         self.assertTrue(any([(recipe.name in response)
             for recipe in Recipe.objects.all()]))
+
+    def test_recipe_description_newlines_are_converted_to_br(self):
+        factories.RecipeFactory(description='line1\nline2')
+        response = self.get_homepage_content().content.decode()
+
+        self.assertIn('line1<br />line2', response)
 
 
 class EveryDayNewRecipeTest(TestCase):
--- recipes/views.py
+++ recipes/views.py
@@ -4,8 +4,11 @@ from django.shortcuts import render
 from recipes.models import Recipe
 
 def todays_recipe(day=datetime.date.today()):
-    target = (day.month * day.day + day.year) % Recipe.objects.count()
-    return Recipe.objects.all()[target]
+    try:
+        target = (day.month * day.day + day.year) % Recipe.objects.count()
+        return Recipe.objects.all()[target]
+    except:
+        pass
 
 def home_page(request):
     recipe = todays_recipe()
