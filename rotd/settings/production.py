69642fbfa143d475b3dcc548bffbda8a6dd6c680
XeryusTC <armadillo@onenetbeyond.org>
2015-08-06 22:03:56 +0200

    Enable template caching in production

--- rotd/settings/production.py
+++ rotd/settings/production.py
@@ -27,3 +27,24 @@ EMAIL_HOST_PASSWORD = get_env_setting('ROTD_EMAIL_HOST_PASSWORD')
 EMAIL_HOST_USER = get_env_setting('ROTD_EMAIL_HOST_USER')
 EMAIL_PORT = get_env_setting('ROTD_EMAIL_PORT')
 EMAIL_USE_TLS = True
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.abspath(os.path.join(BASE_DIR, 'templates'))],
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            'loaders': [
+                ('django.template.loaders.cached.Loader', [
+                    'django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader',
+                ]),
+            ],
+        },
+    },
+]
